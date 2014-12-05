3489e63da1069f28f97b5f39d694273057b8a65e
Christian Mayer <christian@fox21.at>
2014-12-05 21:44:34 +0100

    Fix.

--- lib/FindOpenMP.cmake
+++ lib/FindOpenMP.cmake
@@ -8,22 +8,17 @@
 MESSAGE(STATUS "Find OpenMP header files")
 find_path(OpenMP_HEADER_INCLUDE_PATH omp.h
 	PATHS
-		/usr/local/include
 		/usr/lib/gcc/x86_64-linux-gnu/4.8/include
 		/usr/lib/gcc/x86_64-linux-gnu/4.7/include
 		/usr/lib/gcc/x86_64-linux-gnu/4.6/include
-		#/usr/local/Cellar/gcc49/4.9.2/lib/gcc/x86_64-apple-darwin14.0.0/4.9.2/include
 )
 
 MESSAGE(STATUS "Find OpenMP library files")
 find_library(OpenMP_LIBRARY_PATH gomp
 	PATHS
-		/usr/local/lib
-		#/usr/lib/x86_64-linux-gnu
-		#/usr/lib/gcc/x86_64-linux-gnu/4.8
-		#/usr/lib/gcc/x86_64-linux-gnu/4.7
-		#/usr/lib/gcc/x86_64-linux-gnu/4.6
-		#/usr/local/Cellar/gcc49/4.9.2/lib/gcc/x86_64-apple-darwin14.0.0/4.9.2
+		/usr/lib/gcc/x86_64-linux-gnu/4.8
+		/usr/lib/gcc/x86_64-linux-gnu/4.7
+		/usr/lib/gcc/x86_64-linux-gnu/4.6
 )
 
 if(OpenMP_HEADER_INCLUDE_PATH AND OpenMP_LIBRARY_PATH)
