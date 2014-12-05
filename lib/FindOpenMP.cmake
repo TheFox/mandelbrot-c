5e582cb272ee96a1030f66da5d9fe9cac1b013a5
Christian Mayer <christian@fox21.at>
2014-12-05 19:10:06 +0100

    Brew paths.

--- lib/FindOpenMP.cmake
+++ lib/FindOpenMP.cmake
@@ -10,7 +10,7 @@ find_path(OpenMP_HEADER_INCLUDE_PATH omp.h
 	PATHS
 		/usr/lib/gcc/x86_64-linux-gnu/4.7/include
 		/usr/lib/gcc/x86_64-linux-gnu/4.6/include
-		/usr/local/Cellar/gcc49/4.9.2/lib/gcc/x86_64-apple-darwin14.0.0/4.9.2/include
+		#/usr/local/Cellar/gcc49/4.9.2/lib/gcc/x86_64-apple-darwin14.0.0/4.9.2/include
 )
 
 MESSAGE(STATUS "Find OpenMP library files")
