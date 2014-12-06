d6aa841a80e11a9c57a1895f8f8436e330366532
Christian Mayer <christian@fox21.at>
2014-12-06 18:15:04 +0100

    Use log().
    But it doesn't really bring any speed improvements.

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -99,6 +99,14 @@ int main(int argc, char const *argv[]){
 #endif
 	printf("\n");
 	
+	printf("USE_LOG: ");
+#ifdef USE_LOG
+	printf("yes");
+#else
+	printf("no");
+#endif
+	printf("\n");
+	
 	printf("GRID: ");
 #ifdef GRID
 	printf("yes");
@@ -551,10 +559,13 @@ int point_iteration(const mbnum cx, const mbnum cy, const int depth_max){
 	
 	mbnum x_sqr = x * x;
 	mbnum y_sqr = y * y;
+	
+#ifdef USE_LOG
 	mbnum log_x;
 	mbnum log_y;
-	mbnum x_sqr2;
-	mbnum y_sqr2;
+#endif
+	//mbnum x_sqr2;
+	//mbnum y_sqr2;
 	for(step = 0; step < depth_max && x_sqr + y_sqr < 4.0; step++){
 		y = x * y;
 		y += y;
@@ -562,19 +573,52 @@ int point_iteration(const mbnum cx, const mbnum cy, const int depth_max){
 		
 		x = x_sqr - y_sqr + cx;
 		
+		//if(x > 0 && y > 0)
+		//x_sqr = x * x;
+		//y_sqr = y * y;
+		
+#ifdef USE_LOG
+		/*if(x < 0){
+			x_sqr = x * x;
+		}
+		else{
+			log_x = log(x);
+			x_sqr = exp(log_x + log_x);
+		}
+		
+		if(y < 0){
+			y_sqr = y * y;
+		}
+		else{
+			log_y = log(y);
+			y_sqr = exp(log_y + log_y);
+		}*/
+		
+		if(x < 0)
+			x = abs(x);
+		log_x = log(x);
+		x_sqr = exp(log_x + log_x);
+		
+		if(y < 0)
+			y = abs(y);
+		log_y = log(y);
+		y_sqr = exp(log_y + log_y);
+		
+#else
 		x_sqr = x * x;
 		y_sqr = y * y;
+#endif
+		
+		
+		//x_sqr = exp(log_x + log_x);
+		//y_sqr = exp(log_y + log_y);
 		
 		/*
-		log_x = log(x);
-		log_y = log(y);
-		x_sqr2 = exp(log_x + log_x);
-		y_sqr2 = exp(log_y + log_y);
-		
-		printf("point: %f %f\n", cx, cy);
+		printf("point: %f %f - %f %f\n", cx, cy, x, y);
 		printf("\t A %f %f\n", x_sqr, y_sqr);
 		printf("\t B %f %f %d\n\n", x_sqr2, y_sqr2, x_sqr == x_sqr2 && y_sqr == y_sqr2 ? 1 : 0);
 		*/
+		
 	}
 	
 	return step;
