51fe9865fd1b879538436603a1e21b1267c13cf3
Christian Mayer <christian@fox21.at>
2014-12-07 23:26:39 +0100

    Check OpenMP.

--- src/init_image_plain.h
+++ src/init_image_plain.h
@@ -3,7 +3,9 @@ printf("alloc image_plain\n");
 mbnum **image_plain = (mbnum **)malloc(image_plain_s);
 memset(image_plain, 0, image_plain_s);
 image_plain_size += image_plain_s;
+#ifdef USE_OPENMP
 #pragma omp parallel for
+#endif
 for(pos_x = 0; pos_x < image_width; pos_x++){
 	image_plain[pos_x] = (mbnum *)malloc(image_plain_width_s);
 	memset(image_plain[pos_x], 0, image_plain_width_s);
--- src/init_mb_xy_grid.h
+++ src/init_mb_xy_grid.h
@@ -6,11 +6,15 @@
 	printf("mb_x_grid_s: %lu\n", mb_x_grid_s);
 	printf("mb_y_grid_s: %lu\n", mb_y_grid_s);
 	
+#ifdef USE_OPENMP
 	#pragma omp parallel for
+#endif
 	for(pos_x = 0; pos_x < image_width; pos_x++)
 		mb_x_grid_w[pos_x] = mb_width_min + mb_width_step * pos_x;
 	
+#ifdef USE_OPENMP
 	#pragma omp parallel for
+#endif
 	for(pos_y = 0; pos_y < image_height; pos_y++)
 		mb_y_grid_w[pos_y] = mb_height_min + mb_height_step * (image_height - pos_y);
 	
